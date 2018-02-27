0b53c77457880b507bb638835ee0c367b95ebd37
RinatB2017 <tux4096@gmail.com>
2018-02-28 00:00:33 +0300

    update

--- sketchbook/Murat/WS2811_matrix_test/WS2811_matrix_test.ino
+++ sketchbook/Murat/WS2811_matrix_test/WS2811_matrix_test.ino
@@ -5,24 +5,55 @@
 //---------------------------------------------------------------
 #define NUM_STRIPS 9
 //---------------------------------------------------------------
-int leds_per_strip[NUM_STRIPS] = { 20, 30, 36, 40, 36, 30, 25, 20, 25 };
+enum {
+  RED = 0,
+  GREEN = 1,
+  BLUE = 2
+};
 //---------------------------------------------------------------
-CRGB matrix0[20];
-CRGB matrix1[30];
-CRGB matrix2[36];
-CRGB matrix3[40];
-CRGB matrix4[36];
-CRGB matrix5[30];
-CRGB matrix6[25];
-CRGB matrix7[20];
-CRGB matrix8[25];
+/*
+   0 - 20
+   1 - 30
+   2 - 36
+   3 - 40
+   4 - 36
+   5 - 36
+   6 - 25
+   7 - 20
+   8 - 25
+*/
+//---------------------------------------------------------------
+const int leds_per_strip[NUM_STRIPS] = { 20, 30, 36, 40, 36, 30, 25, 20, 25 };
+//---------------------------------------------------------------
+// левая сторона
+//CRGB matrix0[20];
+//CRGB matrix5[30];
+//CRGB matrix8[25];
+//CRGB matrix4[36];
 
+// правая сторона
+//CRGB matrix7[20];
+//CRGB matrix1[30];
+//CRGB matrix6[25];
+//CRGB matrix2[36];
+
+// горизонталь
+//CRGB matrix3[40];
+//---------------------------------------------------------------
+const int size_left_line  = (20 + 30 + 25 + 36 + 20);
+const int size_right_line = (20 + 30 + 25 + 36 + 20);
+const int size_central_line = 40;
+CRGB left_line[size_left_line];
+CRGB right_line[size_right_line];
+CRGB central_line[size_central_line];
+//---------------------------------------------------------------
 CLEDController *controllers[NUM_STRIPS];
 uint8_t gBrightness = 12;
 //---------------------------------------------------------------
-int index = 0;
-int index_strip = 0;
-//int led_color = 0;
+int current_color = RED;
+int led_color = CRGB::Red;
+//---------------------------------------------------------------
+int current_led = 0;    // текущий светодиод
 //---------------------------------------------------------------
 #define LED_PIN_1 2
 #define LED_PIN_2 3
@@ -34,203 +65,116 @@ int index_strip = 0;
 #define LED_PIN_8 9
 #define LED_PIN_9 10
 //---------------------------------------------------------------
-void setup()
+void debug(String text)
 {
-  Serial.begin(9600);
-
-  controllers[0] = &FastLED.addLeds<WS2812, LED_PIN_1, GRB>(matrix0, leds_per_strip[0]);
-  controllers[1] = &FastLED.addLeds<WS2812, LED_PIN_2, GRB>(matrix1, leds_per_strip[1]);
-  controllers[2] = &FastLED.addLeds<WS2812, LED_PIN_3, GRB>(matrix2, leds_per_strip[2]);
-  controllers[3] = &FastLED.addLeds<WS2812, LED_PIN_4, GRB>(matrix3, leds_per_strip[3]);
-  controllers[4] = &FastLED.addLeds<WS2812, LED_PIN_5, GRB>(matrix4, leds_per_strip[4]);
-  controllers[5] = &FastLED.addLeds<WS2812, LED_PIN_6, GRB>(matrix5, leds_per_strip[5]);
-  controllers[6] = &FastLED.addLeds<WS2812, LED_PIN_7, GRB>(matrix6, leds_per_strip[6]);
-  controllers[7] = &FastLED.addLeds<WS2812, LED_PIN_8, GRB>(matrix7, leds_per_strip[7]);
-  controllers[8] = &FastLED.addLeds<WS2812, LED_PIN_9, GRB>(matrix8, leds_per_strip[8]);
+  Serial.println(text);
 }
 //---------------------------------------------------------------
-/*
-  void test(void)
-  {
-    if (index < NUM_LEDS_PER_STRIP)
-    {
-    index++;
-    }
-    else
-    {
-    index = 0;
-    //    led_color = 2;
-    if (led_color < 2)
-    {
-      led_color++;
-    }
-    else
-    {
-      led_color = 0;
-    }
-    }
-    for (int y = 0; y < NUM_STRIPS; y++)
-    {
-    for (int x = 0; x < NUM_LEDS_PER_STRIP; x++) matrix[y][x] = 0;
-    //    if(led_color == 0)  matrix[y][index] = CRGB::Red;
-    //    if(led_color == 1)  matrix[y][index] = CRGB::Green;
-    //    if(led_color == 2)  matrix[y][index] = CRGB::Blue;
-    switch (led_color)
-    {
-      case 0:
-        matrix[y][index] = CRGB::Red;
-        break;
-      case 1:
-        matrix[y][index] = CRGB::Green;
-        break;
-      case 2:
-        matrix[y][index] = CRGB::Blue;
-        break;
-    }
-    }
+void init_matrix(void)
+{
+  int index = 0;
+  controllers[0] = &FastLED.addLeds<WS2812, LED_PIN_1, GRB>(&left_line[index], leds_per_strip[0]);
+  index += leds_per_strip[0];
+  controllers[5] = &FastLED.addLeds<WS2812, LED_PIN_6, GRB>(&left_line[index], leds_per_strip[5]);
+  index += leds_per_strip[5];
+  controllers[8] = &FastLED.addLeds<WS2812, LED_PIN_9, GRB>(&left_line[index], leds_per_strip[8]);
+  index += leds_per_strip[8];
+  controllers[4] = &FastLED.addLeds<WS2812, LED_PIN_5, GRB>(&left_line[index], leds_per_strip[4]);
 
-    controllers[0]->show(matrix[0], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[1]->show(matrix[1], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[2]->show(matrix[2], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[3]->show(matrix[3], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[4]->show(matrix[4], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[5]->show(matrix[5], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[6]->show(matrix[6], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[7]->show(matrix[7], NUM_LEDS_PER_STRIP, gBrightness);
-    controllers[8]->show(matrix[8], NUM_LEDS_PER_STRIP, gBrightness);
+  index = 0;
+  controllers[7] = &FastLED.addLeds<WS2812, LED_PIN_8, GRB>(&right_line[index], leds_per_strip[7]);
+  index += leds_per_strip[7];
+  controllers[1] = &FastLED.addLeds<WS2812, LED_PIN_2, GRB>(&right_line[index], leds_per_strip[1]);
+  index += leds_per_strip[1];
+  controllers[6] = &FastLED.addLeds<WS2812, LED_PIN_7, GRB>(&right_line[index], leds_per_strip[6]);
+  index += leds_per_strip[6];
+  controllers[2] = &FastLED.addLeds<WS2812, LED_PIN_3, GRB>(&right_line[index], leds_per_strip[2]);
 
-    //delay(50);
-  }
-*/
+  index = 0;
+  controllers[3] = &FastLED.addLeds<WS2812, LED_PIN_4, GRB>(&central_line[index], leds_per_strip[3]);
+}
 //---------------------------------------------------------------
-/*
-   0 - 20
-   1 - 30
-   2 - 36
-   3 - 40
-   4 - 36
-   5 - 36
-   6 - 25
-   7 - 20
-   8 -
-*/
+void show_leds(void)
+{
+  int index = 0;
+  controllers[0]->show(&left_line[index], leds_per_strip[0], gBrightness);
+  index += leds_per_strip[0];
+  controllers[5]->show(&left_line[index], leds_per_strip[5], gBrightness);
+  index += leds_per_strip[5];
+  controllers[8]->show(&left_line[index], leds_per_strip[8], gBrightness);
+  index += leds_per_strip[8];
+  controllers[4]->show(&left_line[index], leds_per_strip[4], gBrightness);
+
+  index = 0;
+  controllers[7]->show(&right_line[index], leds_per_strip[7], gBrightness);
+  index += leds_per_strip[7];
+  controllers[1]->show(&right_line[index], leds_per_strip[1], gBrightness);
+  index += leds_per_strip[1];
+  controllers[6]->show(&right_line[index], leds_per_strip[6], gBrightness);
+  index += leds_per_strip[6];
+  controllers[2]->show(&right_line[index], leds_per_strip[2], gBrightness);
+
+  index = 0;
+  controllers[3]->show(&central_line[index], leds_per_strip[3], gBrightness);
+}
+//---------------------------------------------------------------
+void clear_matrix(void)
+{
+  int x = 0;
+  for (x = 0; x < size_left_line; x++)    left_line[x]    = CRGB::Black;
+  for (x = 0; x < size_right_line; x++)   right_line[x]   = CRGB::Black;
+  for (x = 0; x < size_central_line; x++) central_line[x] = CRGB::Black;
+}
+//---------------------------------------------------------------
+void setup()
+{
+  Serial.begin(57600);
+  delay(1000);
+  init_matrix();
+}
 //---------------------------------------------------------------
 void loop(void)
 {
-  /*
-    Serial.print(index);
-    Serial.print(" ");
-    Serial.println(leds_per_strip[0]);
-  */
-
-  for (int x = 0; x < leds_per_strip[0]; x++) matrix0[x] = 0;
-  for (int x = 0; x < leds_per_strip[1]; x++) matrix1[x] = 0;
-  for (int x = 0; x < leds_per_strip[2]; x++) matrix2[x] = 0;
-  for (int x = 0; x < leds_per_strip[3]; x++) matrix3[x] = 0;
-  for (int x = 0; x < leds_per_strip[4]; x++) matrix4[x] = 0;
-  for (int x = 0; x < leds_per_strip[5]; x++) matrix5[x] = 0;
-  for (int x = 0; x < leds_per_strip[6]; x++) matrix6[x] = 0;
-  for (int x = 0; x < leds_per_strip[7]; x++) matrix7[x] = 0;
-  for (int x = 0; x < leds_per_strip[8]; x++) matrix8[x] = 0;
-
-  if (index == 0)
+  debug("loop");
+  clear_matrix();
+  if (current_led < 111) // без горизонта
   {
-    if (index_strip < 8)
+    left_line[current_led]  = led_color;
+    right_line[current_led] = led_color;
+    show_leds();
+    current_led++;
+  }
+  else if (current_led < (111 + 20)) //горизонт
+  {
+    current_led++;
+  }
+  else
+  {
+    current_led = 0;
+    switch (current_color)
     {
-      index_strip++;
-    }
-    else
-    {
-      index_strip = 0;
+      case RED:
+        current_color = GREEN;
+        led_color = CRGB::Green;
+        break;
+        
+      case GREEN:
+        current_color = BLUE;
+        led_color = CRGB::Blue;
+        break;
+        
+      case BLUE:
+        current_color = RED;
+        led_color = CRGB::Red;
+        break;
+        
+      default:
+        current_color = RED;
+        led_color = CRGB::Red;
+        break;
     }
   }
-
-  switch (index_strip)
-  {
-    case 0:
-      if (index < leds_per_strip[0])
-        index++;
-      else
-        index = 0;
-      matrix0[index] = CRGB::Red;
-      break;
-
-    case 1:
-      if (index < leds_per_strip[1])
-        index++;
-      else
-        index = 0;
-      matrix1[index] = CRGB::Red;
-      break;
-
-    case 2:
-      if (index < leds_per_strip[2])
-        index++;
-      else
-        index = 0;
-      matrix2[index] = CRGB::Red;
-      break;
-
-    case 3:
-      if (index < leds_per_strip[3])
-        index++;
-      else
-        index = 0;
-      matrix3[index] = CRGB::Red;
-      break;
-
-    case 4:
-      if (index < leds_per_strip[4])
-        index++;
-      else
-        index = 0;
-      matrix4[index] = CRGB::Red;
-      break;
-
-    case 5:
-      if (index < leds_per_strip[5])
-        index++;
-      else
-        index = 0;
-      matrix5[index] = CRGB::Red;
-      break;
-
-    case 6:
-      if (index < leds_per_strip[6])
-        index++;
-      else
-        index = 0;
-      matrix6[index] = CRGB::Red;
-      break;
-
-    case 7:
-      if (index < leds_per_strip[7])
-        index++;
-      else
-        index = 0;
-      matrix7[index] = CRGB::Red;
-      break;
-
-    case 8:
-      if (index < leds_per_strip[8])
-        index++;
-      else
-        index = 0;
-      matrix8[index] = CRGB::Red;
-      break;
-  }
-
-  controllers[0]->show(matrix0, leds_per_strip[0], gBrightness);
-  controllers[1]->show(matrix1, leds_per_strip[1], gBrightness);
-  controllers[2]->show(matrix2, leds_per_strip[2], gBrightness);
-  controllers[3]->show(matrix3, leds_per_strip[3], gBrightness);
-  controllers[4]->show(matrix4, leds_per_strip[4], gBrightness);
-  controllers[5]->show(matrix5, leds_per_strip[5], gBrightness);
-  controllers[6]->show(matrix6, leds_per_strip[6], gBrightness);
-  controllers[7]->show(matrix7, leds_per_strip[7], gBrightness);
-  controllers[8]->show(matrix8, leds_per_strip[8], gBrightness);
-
-  delay(100);
+  delay(10);
 }
 //---------------------------------------------------------------
 
---
+++ sketchbook/Murat/WS2811_matrix_test_reserv/WS2811_matrix_test_reserv.ino
@@ -0,0 +1,246 @@
+//---------------------------------------------------------------
+// тест по-быстрому
+//---------------------------------------------------------------
+#include "FastLED.h"
+//---------------------------------------------------------------
+#define NUM_STRIPS 9
+//---------------------------------------------------------------
+enum {
+  RED = 0,
+  GREEN,
+  BLUE
+};
+//---------------------------------------------------------------
+/*
+   0 - 20
+   1 - 30
+   2 - 36
+   3 - 40
+   4 - 36
+   5 - 36
+   6 - 25
+   7 - 20
+   8 - 25
+*/
+//---------------------------------------------------------------
+int leds_per_strip[NUM_STRIPS] = { 20, 30, 36, 40, 36, 30, 25, 20, 25 };
+//---------------------------------------------------------------
+CRGB matrix0[20];
+CRGB matrix1[30];
+CRGB matrix2[36];
+CRGB matrix3[40];
+CRGB matrix4[36];
+CRGB matrix5[30];
+CRGB matrix6[25];
+CRGB matrix7[20];
+CRGB matrix8[25];
+//---------------------------------------------------------------
+CLEDController *controllers[NUM_STRIPS];
+uint8_t gBrightness = 12;
+//---------------------------------------------------------------
+int current_led = 0;    // текущий светодиод
+int current_strip = 0;  // текущая лента светодиодов
+//---------------------------------------------------------------
+CRGB led_color = CRGB::Red;
+int current_color = RED;
+//---------------------------------------------------------------
+#define LED_PIN_1 2
+#define LED_PIN_2 3
+#define LED_PIN_3 4
+#define LED_PIN_4 5
+#define LED_PIN_5 6
+#define LED_PIN_6 7
+#define LED_PIN_7 8
+#define LED_PIN_8 9
+#define LED_PIN_9 10
+//---------------------------------------------------------------
+//void debug(String text)
+//{
+//  Serial.println(text);
+//}
+//---------------------------------------------------------------
+void init_matrix(void)
+{
+  //debug("init_matrix");
+  controllers[0] = &FastLED.addLeds<WS2812, LED_PIN_1, GRB>(matrix0, leds_per_strip[0]);
+  controllers[1] = &FastLED.addLeds<WS2812, LED_PIN_2, GRB>(matrix1, leds_per_strip[1]);
+  controllers[2] = &FastLED.addLeds<WS2812, LED_PIN_3, GRB>(matrix2, leds_per_strip[2]);
+  controllers[3] = &FastLED.addLeds<WS2812, LED_PIN_4, GRB>(matrix3, leds_per_strip[3]);
+  controllers[4] = &FastLED.addLeds<WS2812, LED_PIN_5, GRB>(matrix4, leds_per_strip[4]);
+  controllers[5] = &FastLED.addLeds<WS2812, LED_PIN_6, GRB>(matrix5, leds_per_strip[5]);
+  controllers[6] = &FastLED.addLeds<WS2812, LED_PIN_7, GRB>(matrix6, leds_per_strip[6]);
+  controllers[7] = &FastLED.addLeds<WS2812, LED_PIN_8, GRB>(matrix7, leds_per_strip[7]);
+  controllers[8] = &FastLED.addLeds<WS2812, LED_PIN_9, GRB>(matrix8, leds_per_strip[8]);
+}
+//---------------------------------------------------------------
+void show_leds(void)
+{
+  controllers[0]->show(matrix0, leds_per_strip[0], gBrightness);
+  controllers[1]->show(matrix1, leds_per_strip[1], gBrightness);
+  controllers[2]->show(matrix2, leds_per_strip[2], gBrightness);
+  controllers[3]->show(matrix3, leds_per_strip[3], gBrightness);
+  controllers[4]->show(matrix4, leds_per_strip[4], gBrightness);
+  controllers[5]->show(matrix5, leds_per_strip[5], gBrightness);
+  controllers[6]->show(matrix6, leds_per_strip[6], gBrightness);
+  controllers[7]->show(matrix7, leds_per_strip[7], gBrightness);
+  controllers[8]->show(matrix8, leds_per_strip[8], gBrightness);
+}
+//---------------------------------------------------------------
+void clear_matrix(void)
+{
+  for (int x = 0; x < leds_per_strip[0]; x++) matrix0[x] = 0;
+  for (int x = 0; x < leds_per_strip[1]; x++) matrix1[x] = 0;
+  for (int x = 0; x < leds_per_strip[2]; x++) matrix2[x] = 0;
+  for (int x = 0; x < leds_per_strip[3]; x++) matrix3[x] = 0;
+  for (int x = 0; x < leds_per_strip[4]; x++) matrix4[x] = 0;
+  for (int x = 0; x < leds_per_strip[5]; x++) matrix5[x] = 0;
+  for (int x = 0; x < leds_per_strip[6]; x++) matrix6[x] = 0;
+  for (int x = 0; x < leds_per_strip[7]; x++) matrix7[x] = 0;
+  for (int x = 0; x < leds_per_strip[8]; x++) matrix8[x] = 0;
+}
+//---------------------------------------------------------------
+void show_line(void)
+{
+  switch (current_strip)
+  {
+    case 0:
+      matrix0[current_led] = led_color;
+      break;
+
+    case 1:
+      matrix1[current_led] = led_color;
+      break;
+
+    case 2:
+      matrix2[current_led] = led_color;
+      break;
+
+    case 3:
+      matrix3[current_led] = led_color;
+      break;
+
+    case 4:
+      matrix4[current_led] = led_color;
+      break;
+
+    case 5:
+      matrix5[current_led] = led_color;
+      break;
+
+    case 6:
+      matrix6[current_led] = led_color;
+      break;
+
+    case 7:
+      matrix7[current_led] = led_color;
+      break;
+
+    case 8:
+      matrix8[current_led] = led_color;
+      break;
+  }
+  show_leds();
+}
+//---------------------------------------------------------------
+void switch_color(void)
+{
+  //debug("current_strip = " + String(current_strip));
+  switch (current_color)
+  {
+    case RED:
+      //debug("Red");
+      led_color = CRGB::Green;
+      current_color = GREEN;
+      break;
+
+    case GREEN:
+      //debug("Green");
+      led_color = CRGB::Blue;
+      current_color = BLUE;
+      break;
+
+    case BLUE:
+      //debug("Blue");
+      led_color = CRGB::Red;
+      current_color = RED;
+      break;
+
+    default:
+      //debug("default");
+      led_color = CRGB::Red;
+      current_color = RED;
+      break;
+  }
+}
+//---------------------------------------------------------------
+void switch_strip(void)
+{
+  if (current_led != 0)
+  {
+    return;
+  }
+  switch (current_strip)
+  {
+    case 0:
+      current_strip = 5;
+      break;
+    case 1:
+      current_strip = 6;
+      break;
+    case 2:
+      current_strip = 3;
+      break;
+    case 3:
+      current_strip = 0;
+      break;
+    case 4:
+      current_strip = 7;
+      break;
+    case 5:
+      current_strip = 8;
+      break;
+    case 6:
+      current_strip = 2;
+      break;
+    case 7:
+      current_strip = 1;
+      break;
+    case 8:
+      current_strip = 4;
+      break;
+
+    default:
+      current_strip = 0;
+      break;
+  }
+  current_led = 0;
+  if (current_strip == 0)
+  {
+    switch_color();
+  }
+}
+//---------------------------------------------------------------
+void setup()
+{
+  Serial.begin(57600);
+  init_matrix();
+}
+//---------------------------------------------------------------
+void loop(void)
+{
+  clear_matrix();
+  switch_strip();
+
+  if (current_led < leds_per_strip[current_strip])
+  {
+    show_line();
+    current_led++;
+    //delay(10);
+  }
+  else
+  {
+    current_led = 0;
+  }
+}
+//---------------------------------------------------------------
+
