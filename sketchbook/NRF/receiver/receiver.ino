812cab616b20afe41b4186008de34c14552ec8e3
RinatB2017 <tux4096@gmail.com>
2018-06-22 21:58:53 +0300

    update

--- sketchbook/NRF/receiver/receiver.ino
+++ sketchbook/NRF/receiver/receiver.ino
@@ -1,15 +1,80 @@
+//---------------------------------------------------------------
 // приемник
-
+//---------------------------------------------------------------
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+//---------------------------------------------------------------
 RF24 radio(9, 10);
+//---------------------------------------------------------------
+//адресные пины
+int address_pin_0 = 7;
+int address_pin_1 = 8;
 
-void setup() {
+//пин шима
+int pin_output = 8;
+//--------------------------------------------------------------------------------
+#pragma pack (push, 1)
+
+union U_BYTE
+{
+  uint8_t value;
+  struct
+  {
+    uint8_t bit0: 1;
+    uint8_t bit1: 1;
+    uint8_t bit2: 1;
+    uint8_t bit3: 1;
+    uint8_t bit4: 1;
+    uint8_t bit5: 1;
+    uint8_t bit6: 1;
+    uint8_t bit7: 1;
+  } bites;
+};
+
+//typedef struct PACKET
+//{
+//  uint8_t marker_begin;
+//  uint8_t data[4];
+//  uint8_t marker_end;
+//} packet_t;
+//
+//union UINT16
+//{
+//  uint16_t value;
+//  struct {
+//    uint8_t a;
+//    uint8_t b;
+//    uint8_t c;
+//    uint8_t d;
+//  } bytes;
+//};
+
+#pragma pack(pop)
+//---------------------------------------------------------------
+void init_gpio()
+{
+  pinMode(address_pin_0,  INPUT_PULLUP);
+  pinMode(address_pin_1,  INPUT_PULLUP);
+
+  pinMode(pin_output, OUTPUT);
+}
+//---------------------------------------------------------------
+int get_address()
+{
+  union U_BYTE temp;
+  temp.value = 0;
+  temp.bites.bit0 = (digitalRead(address_pin_0) == LOW);
+  temp.bites.bit1 = (digitalRead(address_pin_1) == LOW);
+  return temp.value;
+}
+//---------------------------------------------------------------
+void init_receiver()
+{
   // инициализация nRF24L01+
   radio.begin();
   // задаем канал для приёма данных. Идентично передатчику
-  radio.setChannel(0);
+  radio.setChannel(get_address());
   // Скорость передачи данных. Идентично передатчику
   radio.setDataRate (RF24_1MBPS);
   // Мощность передатчика. Идентично передатчику
@@ -18,26 +83,29 @@ void setup() {
   radio.openReadingPipe (1, 0x1234567890LL);
   // Включаем приемник и слушаем открытую трубу
   radio.startListening ();
-
+}
+//---------------------------------------------------------------
+void setup()
+{
   // монитор порта
   Serial.begin(9600);
   Serial.println("Init receiver");
-}
 
-void loop() {
-  // массив, для получения данных
-  int data[2] = {};
+  init_gpio();
+  init_receiver();
+}
+//---------------------------------------------------------------
+void loop()
+{
+  uint16_t data = 0;
 
   // если что-то удалось получить
-  if (radio.available()) {
+  if (radio.available())
+  {
     // читаем данные
     radio.read(&data, sizeof(data));
-    // вывод первого элемента массива
-    Serial.print("data[0]: ");
-    Serial.print(data[0]);
-
-    // вывод второго элемента массива
-    Serial.print(" data[1]: ");
-    Serial.println(data[1]);
+    analogWrite(pin_output, data);
   }
 }
+//---------------------------------------------------------------
+
--- sketchbook/NRF/transmitter/transmitter.ino
+++ sketchbook/NRF/transmitter/transmitter.ino
@@ -1,19 +1,115 @@
+//---------------------------------------------------------------
 // передатчик
-
+//---------------------------------------------------------------
 // Библиотека для работы с SPI
 #include <SPI.h>
 // Файл с настройками для библиотеки RF24
 #include <nRF24L01.h>
 // Библиотека RF24
 #include <RF24.h>
-
+//---------------------------------------------------------------
 // 9 и 10 - это номера пинов CE и CSN модуля nRF24L01+
 RF24 radio(9, 10);
+//---------------------------------------------------------------
+//аналоговые пины
+int analog_pin_1 = A0;
+int analog_pin_2 = A1;
+int analog_pin_3 = A2;
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Init transmitter");
+//пины кнопок
+int btn_1_pin = 7;
+int btn_2_pin = 8;
+int btn_3_pin = 9;
+//---------------------------------------------------------------
+int value_pin_1 = 0;
+int value_pin_2 = 0;
+int value_pin_3 = 0;
+//--------------------------------------------------------------------------------
+#pragma pack (push, 1)
 
+//union UINT16
+//{
+//  uint16_t value;
+//  struct {
+//    uint8_t a;
+//    uint8_t b;
+//    uint8_t c;
+//    uint8_t d;
+//  } bytes;
+//};
+
+#pragma pack(pop)
+//---------------------------------------------------------------
+void init_gpio()
+{
+  pinMode(btn_1_pin,  INPUT_PULLUP);
+  pinMode(btn_2_pin,  INPUT_PULLUP);
+  pinMode(btn_3_pin,  INPUT_PULLUP);
+}
+//---------------------------------------------------------------
+void read_data()
+{
+  value_pin_1 = analogRead(analog_pin_1);
+  value_pin_2 = analogRead(analog_pin_2);
+  value_pin_3 = analogRead(analog_pin_3);
+}
+//---------------------------------------------------------------
+bool check_btn_1()
+{
+  return (digitalRead(btn_1_pin) == LOW);
+}
+//---------------------------------------------------------------
+bool check_btn_2()
+{
+  return (digitalRead(btn_2_pin) == LOW);
+}
+//---------------------------------------------------------------
+bool check_btn_3()
+{
+  return (digitalRead(btn_3_pin) == LOW);
+}
+//--------------------------------------------------------------------------------
+void convert_data(uint8_t input, char *output)
+{
+  switch (input)
+  {
+    case 0x00: *output = '0'; break;
+    case 0x01: *output = '1'; break;
+    case 0x02: *output = '2'; break;
+    case 0x03: *output = '3'; break;
+    case 0x04: *output = '4'; break;
+    case 0x05: *output = '5'; break;
+    case 0x06: *output = '6'; break;
+    case 0x07: *output = '7'; break;
+    case 0x08: *output = '8'; break;
+    case 0x09: *output = '9'; break;
+    case 0x0A: *output = 'A'; break;
+    case 0x0B: *output = 'B'; break;
+    case 0x0C: *output = 'C'; break;
+    case 0x0D: *output = 'D'; break;
+    case 0x0E: *output = 'E'; break;
+    case 0x0F: *output = 'F'; break;
+    default: break;
+  }
+}
+//--------------------------------------------------------------------------------
+//void convert_data_to_ascii(uint16_t value,
+//                           uint8_t *a,
+//                           uint8_t *b,
+//                           uint8_t *c,
+//                           uint8_t *d)
+//{
+//  UINT16 data16;
+//  data16.value = value;
+//
+//  convert_data(data16.bytes.a, *a);
+//  convert_data(data16.bytes.b, *b);
+//  convert_data(data16.bytes.c, *c);
+//  convert_data(data16.bytes.d, *d);
+//}
+//---------------------------------------------------------------
+void init_transmitter()
+{
   // инициализация nRF24L01+
   radio.begin();
   // задаем канал для передачи данных (от 0 до 127)
@@ -22,7 +118,7 @@ void setup() {
   // Возможны: RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
   radio.setDataRate (RF24_1MBPS);
   // Мощность передатчика -6dBm.
-  // Возможные можности:
+  // Возможные мощности:
   // RF24_PA_MIN = -18dBm, RF24_PA_LOW = -12dBm,
   // RF24_PA_HIGH = -6dBm, RF24_PA_MAX = 0dBm
   radio.setPALevel (RF24_PA_HIGH);
@@ -31,17 +127,60 @@ void setup() {
   // они должны отличаться только последним байтом
   radio.openWritingPipe (0x1234567890LL);
 }
-
-void loop() {
-  // массив для тестовых данных
-  int data[2] = {0, 0};
-  // читаем значение с аналогового пина для получения тестового числа
-  data[0] = analogRead(0);
-  // второе тестовое значение будет фиксированным
-  data[1] = 80;
-  // отправляем данные
-  radio.write(&data, sizeof(data));
-
-  Serial.println("send data...");
+//---------------------------------------------------------------
+void pause()
+{
+  Serial.println("pause");
   delay(1000);
 }
+//---------------------------------------------------------------
+void send_data(int address, int value)
+{
+  int data[2] = {0, 0};
+
+  radio.setChannel(address);
+
+  //  char a = 0;
+  //  char b = 0;
+  //  char c = 0;
+  //  char d = 0;
+  //
+  //  convert_data_to_ascii(value,
+  //                        &a,
+  //                        &b,
+  //                        &c,
+  //                        &d);
+
+  //PACKET packet;
+  //packet.data = value;
+
+  // отправляем данные
+  radio.write(&value, sizeof(value));
+}
+//---------------------------------------------------------------
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Init transmitter");
+
+  init_gpio();
+  init_transmitter();
+}
+//---------------------------------------------------------------
+void loop()
+{
+  read_data();
+
+  //передаем последовательно на 3 адреса (0..2)
+  if (check_btn_1())
+  {
+    send_data(0, value_pin_1);
+    send_data(1, value_pin_2);
+    send_data(2, value_pin_3);
+  }
+
+  //ждем
+  pause();
+}
+//---------------------------------------------------------------
+
