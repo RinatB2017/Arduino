cbc635710dd0f8e8a2a99aea444fcac1e3205e2f
RinatB2017 <tux4096@gmail.com>
2018-07-05 16:59:06 +0300

    update

---
+++ sketchbook/libraries/WS2811_matrix/ws2811_matrix.cpp
@@ -0,0 +1,11 @@
+#include "ws2811_matrix.h"
+
+WS2811_matrix::WS2811_matrix()
+{
+
+}
+
+int WS2811_matrix::test(void)
+{
+	return 42;
+}
---
+++ sketchbook/libraries/WS2811_matrix/ws2811_matrix.h
@@ -0,0 +1,13 @@
+#ifndef WS2811_MATRIX_H
+#define WS2811_MATRIX_H
+
+
+class WS2811_matrix
+{
+public:
+    WS2811_matrix();
+	
+	int test(void);
+};
+
+#endif // WS2811_MATRIX_H
