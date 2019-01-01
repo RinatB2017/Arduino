8db95b564c084cd690e500dd48e18894c1d8cd6e
RinatB2017 <tux4096@gmail.com>
2019-01-01 13:04:58 +0300

    update

--- sketchbook/year_2019/year_2019.ino
+++ sketchbook/year_2019/year_2019.ino
@@ -5,8 +5,11 @@
 
 #define LED_PIN     9
 #define BRIGHTNESS  10 // maximum brightness 96
-#define DELAY_MS    100
+#define DELAY_MS    50
 #define LED_LEN     9
+
+#define LEN_LEDS        NUM_LEDS
+#define LEN_CLEAN_LEDS  (NUM_LEDS + (LED_LEN * 3))
 
 //#define COLOR_1     CRGB::Magenta
 //#define COLOR_2     CRGB::Green
@@ -14,8 +17,12 @@
 #define COLOR_1     CRGB::Gold
 #define COLOR_2     CRGB::Red
 #define COLOR_3     CRGB::Blue
+
+CRGB current_color = CRGB::Gold;
+int index_color = 0;
 //---------------------------------------------------------------
-CRGB leds[NUM_LEDS + LED_LEN];
+CRGB clean_leds[LEN_CLEAN_LEDS];
+CRGB leds[LEN_LEDS];
 CLEDController *controllers;
 
 int index = 0;
@@ -83,11 +90,67 @@ void update_1()
 void setup()
 {
   controllers = &FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+
+#if 1
+  index = 0;
+  index_color = 0;
+  int temp_index = 0;
+  while (index < LEN_CLEAN_LEDS)
+  {
+    if (temp_index > LED_LEN)
+    {
+      index_color++;
+      temp_index = 0;
+    }
+    else
+    {
+      temp_index++;
+    }
+
+    switch (index_color)
+    {
+      case 0:
+        current_color = COLOR_1;
+        break;
+
+      case 1:
+        current_color = COLOR_2;
+        break;
+
+      case 2:
+        current_color = COLOR_3;
+        break;
+
+      default:
+        index_color = 0;
+        current_color = COLOR_1;
+        break;
+    }
+    clean_leds[index++] = current_color;
+  }
+  index = 0;
+#endif
+}
+//---------------------------------------------------------------
+void update_2()
+{
+  index ++;
+  if (index > (LED_LEN * 3))
+  {
+    index = 0;
+  }
+  for (int n = 0; n < NUM_LEDS; n++)
+  {
+    leds[n] = clean_leds[n + index];
+  }
+  controllers->show(leds, NUM_LEDS, BRIGHTNESS);
+  delay(DELAY_MS);
 }
 //---------------------------------------------------------------
 void loop()
 {
-  update_1();
+  //update_1();
+  update_2();
 }
 //---------------------------------------------------------------
 
