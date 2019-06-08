0dc63e5599d3ae594388c25066e56cecd82a9bc9
RinatB2017 <tux4096@gmail.com>
2019-06-08 12:49:48 +0300

    update

--- sketchbook/WS2811_test/WS2811_test.ino
+++ sketchbook/WS2811_test/WS2811_test.ino
@@ -1,14 +1,14 @@
 //---------------------------------------------------------------
-#include "FastSPI_LED2.h"
+#include "FastLED.h"
 //---------------------------------------------------------------
 #if 1
-# define NUM_LEDS_1  5
+# define NUM_LEDS  6
 #else
-# define NUM_LEDS_1  144*4
+# define NUM_LEDS  144*4
 #endif
 
-#define LED_PIN_1   8
-#define BRIGHTNESS  255 // maximum brightness 96
+#define LED_PIN     9
+#define BRIGHTNESS  64
 #define DELAY_MS    50
 //---------------------------------------------------------------
 enum {
@@ -17,100 +17,71 @@ enum {
   BLUE
 };
 //---------------------------------------------------------------
-CRGB leds1[NUM_LEDS_1 + 1];
+CRGB leds[NUM_LEDS + 1];
 //---------------------------------------------------------------
-int index_1 = 0;
-int state_1 = RED;
+int index = 0;
+int state = RED;
 bool flag = false;
 //---------------------------------------------------------------
 void setup()
 {
   delay(2000);
   LEDS.setBrightness(BRIGHTNESS);
-  LEDS.addLeds<WS2811, LED_PIN_1, RGB>(leds1, NUM_LEDS_1);
-
-#if 1  
-  clear_1();
-
-//  CRGB led;
-//  led.r = 255;
-//  led.g = 255;
-//  led.b = 255;
-//
-//  leds1[0] = led;
-//  leds1[4] = led;
-
-  leds1[0] = CRGB::Red;
-  leds1[1] = CRGB::Green;
-  leds1[2] = CRGB::Blue;
-  
-  LEDS.show();
-#endif
-  
-#if 0
-  for (int n = 0; n < NUM_LEDS_1; n++)
-  {
-    leds1[n] = 0;
-  }
-#endif
+  LEDS.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS);
 }
 //---------------------------------------------------------------
 void clear_1(void)
 {
-  for (int n = 0; n < NUM_LEDS_1; n++)
+  for (int n = 0; n < NUM_LEDS; n++)
   {
-    leds1[n] = 0;
+    leds[n] = 0;
   }
 }
 //---------------------------------------------------------------
 void update_1(void)
 {
-  if (index_1 == 0)
+  if (index == 0)
   {
-    switch (state_1)
+    switch (state)
     {
       case RED:
-        state_1 = GREEN;
+        state = GREEN;
         break;
       case GREEN:
-        state_1 = BLUE;
+        state = BLUE;
         break;
       case BLUE:
-        state_1 = RED;
+        state = RED;
         break;
       default:
         break;
     }
   }
 
-  switch (state_1)
+  switch (state)
   {
     case RED:
-      leds1[index_1] = CRGB::Red;
+      leds[index] = CRGB::Red;
       break;
     case GREEN:
-      leds1[index_1] = CRGB::Green;
+      leds[index] = CRGB::Green;
       break;
     case BLUE:
-      leds1[index_1] = CRGB::Blue;
+      leds[index] = CRGB::Blue;
       break;
     default:
       break;
   }
 
-  if (index_1 < NUM_LEDS_1) index_1++;
-  else index_1 = 0;
+  if (index < NUM_LEDS) index++;
+  else index = 0;
 }
 //---------------------------------------------------------------
 void loop()
 {
-#if 0  
   clear_1();
   update_1();
   LEDS.show();
   delay(DELAY_MS);
-#endif
 }
 //---------------------------------------------------------------
-
-
